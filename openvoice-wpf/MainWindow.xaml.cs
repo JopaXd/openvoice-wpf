4274906a338ce00a03b83cb285eb5cb96716a49b
Pavle Milanov <pavle_milanov@hotmail.com>
2021-10-17 00:26:05 +0200

    Added the functionality to change the audio device.

--- openvoice-wpf/MainWindow.xaml.cs
+++ openvoice-wpf/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using InTheHand.Net;
+using NAudio.Wave;
 
 namespace openvoice_wpf
 {
@@ -25,11 +26,13 @@ namespace openvoice_wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        string configPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/open_voice/config.json";
+
+        string configDir = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/open_voice/";
+
         public MainWindow()
         {
-            string configPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/open_voice/config.json";
-
-            string configDir = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/open_voice/";
+            MMDeviceEnumerator deviceEnumerator = new MMDeviceEnumerator();
 
             if (!Directory.Exists(configDir))
             {
@@ -38,7 +41,6 @@ namespace openvoice_wpf
 
             if (!File.Exists(configPath))
             {
-                MMDeviceEnumerator deviceEnumerator = new MMDeviceEnumerator();
                 JObject data = new JObject();
                 JProperty conns = new JProperty("connections", new JArray());
                 JProperty audioDevice = new JProperty("audioDevice", deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console).FriendlyName);
@@ -53,6 +55,35 @@ namespace openvoice_wpf
                 }
             }
             InitializeComponent();
+            List<string> aDevices = getAudioDevices();
+            string defaultAudioDevice = getCurrentAudioDevice();
+            int deviceIndex = aDevices.FindIndex(a => a == defaultAudioDevice);
+            if (deviceIndex == -1)
+            {
+                //Meaning the default device was not found, select the system default one.
+                deviceIndex = aDevices.FindIndex(a => a == deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console).FriendlyName);
+            }
+            foreach (string device in aDevices) {
+                audioDeviceCBox.Items.Add(device);
+            }
+            audioDeviceCBox.SelectedIndex = deviceIndex;
+        }
+
+        private List<string> getAudioDevices() {
+            List<string> audioDevices = new List<string> { };
+            for (int n = -1; n < WaveOut.DeviceCount; n++)
+            {
+                var caps = WaveOut.GetCapabilities(n);
+                audioDevices.Add(caps.ProductName);
+            }
+            return audioDevices;
+        }
+
+        private string getCurrentAudioDevice()
+        {
+            JObject connFile = JObject.Parse(File.ReadAllText(configPath));
+            string currentAudioDevice = (string)connFile["audioDevice"];
+            return currentAudioDevice;
         }
 
         private void createWifiConnBtn_Click(object sender, RoutedEventArgs e)
@@ -71,6 +102,24 @@ namespace openvoice_wpf
             catch { 
                 //Meaning the bluetooth error occured in the AddBtWindow. Do nothing, the user is already informed.
             }
+        }
+
+        private void changeAudioDevice(string name)
+        {
+            JObject connFile = JObject.Parse(File.ReadAllText(configPath));
+            connFile["audioDevice"] = name;
+            using (StreamWriter f = File.CreateText(configPath))
+            using (JsonTextWriter writer = new JsonTextWriter(f))
+            {
+                connFile.WriteTo(writer);
+                writer.Close();
+                f.Close();
+            }
+        }
+
+        private void audioDevice_onChange(object sender, SelectionChangedEventArgs e)
+        {
+            changeAudioDevice(audioDeviceCBox.SelectedItem.ToString());
         }
     }
 }
