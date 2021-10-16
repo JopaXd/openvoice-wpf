25ffacc85887adb8f3eab53a1667d6f34424a289
Pavle Milanov <pavle_milanov@hotmail.com>
2021-10-16 21:20:08 +0200

    Adding Wi-Fi connections now works, but still not visualised. Also added first launch stuff.

---
+++ openvoice-wpf/AddWifiWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace openvoice_wpf
+{
+    /// <summary>
+    /// Interaction logic for AddWifiWindow.xaml
+    /// </summary>
+    public partial class AddWifiWindow : Window
+    {
+        public AddWifiWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void createNewConnection(string type, string name, string addr)
+        {
+            string configPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/open_voice/config.json";
+            JObject connFile = JObject.Parse(File.ReadAllText(configPath));
+            JArray connections = (JArray)connFile["connections"];
+            JObject newConn = new JObject();
+            newConn.Add(new JProperty("type", type));
+            newConn.Add(new JProperty("addr", addr));
+            newConn.Add(new JProperty("name", name));
+            connections.Add(newConn);
+            connFile["connections"] = connections;
+            using (StreamWriter f = File.CreateText(configPath))
+            using (JsonTextWriter writer = new JsonTextWriter(f))
+            {
+                connFile.WriteTo(writer);
+                writer.Close();
+                f.Close();
+            }
+            return;
+        }
+
+        private void createConnBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string ipAddress = this.addressTbox.Text;
+            string connName = this.nameTbox.Text;
+            string connType = "wifi";
+            if (ipAddress != "" && connName != "")
+            {
+                Regex ipv4Regex = new Regex(@"^(?:25[0-5]|2[0-4]\d|[0-1]?\d{1,2})(?:\.(?:25[0-5]|2[0-4]\d|[0-1]?\d{1,2})){3}$");
+                if (ipv4Regex.IsMatch(ipAddress))
+                {
+                    createNewConnection(connType, connName, ipAddress);
+                    this.Close();
+                }
+                else {
+                    MessageBox.Show("You must provide a valid IPv4 address!", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else {
+                MessageBox.Show("You must provide both the name and the ip address!", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
--- openvoice-wpf/MainWindow.xaml.cs
+++ openvoice-wpf/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using NAudio.CoreAudioApi;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,7 +26,38 @@ namespace openvoice_wpf
     {
         public MainWindow()
         {
+            string configPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/open_voice/config.json";
+
+            string configDir = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/open_voice/";
+
+            if (!Directory.Exists(configDir))
+            {
+                Directory.CreateDirectory(configDir);
+            }
+
+            if (!File.Exists(configPath))
+            {
+                MMDeviceEnumerator deviceEnumerator = new MMDeviceEnumerator();
+                JObject data = new JObject();
+                JProperty conns = new JProperty("connections", new JArray());
+                JProperty audioDevice = new JProperty("audioDevice", deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console).FriendlyName);
+                data.Add(conns);
+                data.Add(audioDevice);
+                using (StreamWriter f = File.CreateText(configPath))
+                using (JsonTextWriter writer = new JsonTextWriter(f))
+                {
+                    data.WriteTo(writer);
+                    writer.Close();
+                    f.Close();
+                }
+            }
             InitializeComponent();
+        }
+
+        private void createWifiConnBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddWifiWindow wifiWindow = new AddWifiWindow();
+            wifiWindow.Show();
         }
     }
 }
