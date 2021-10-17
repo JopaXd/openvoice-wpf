e4e9160d7ffe01618f6ee01649179fbfc84679a1
Pavle Milanov <pavle_milanov@hotmail.com>
2021-10-17 12:51:00 +0200

    Connections are now visualised in the app.

--- openvoice-wpf/MainWindow.xaml.cs
+++ openvoice-wpf/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using InTheHand.Net;
 using NAudio.Wave;
+using FontAwesome.WPF;
 
 namespace openvoice_wpf
 {
@@ -67,6 +68,88 @@ namespace openvoice_wpf
                 audioDeviceCBox.Items.Add(device);
             }
             audioDeviceCBox.SelectedIndex = deviceIndex;
+            VisualiseConnections();
+        }
+        private List<List<string>> getConnections()
+        {
+            JObject connFile = JObject.Parse(File.ReadAllText(configPath));
+            JArray connectionsJson = (JArray)connFile["connections"];
+            List<List<string>> connections = new List<List<string>> { };
+            foreach (JObject conn in connectionsJson)
+            {
+                string type = conn["type"].ToString();
+                string name = conn["name"].ToString();
+                string addr = conn["addr"].ToString();
+                List<string> newConn = new List<string> { };
+                newConn.Add(type);
+                newConn.Add(name);
+                newConn.Add(addr);
+                connections.Add(newConn);
+            }
+            return connections;
+        }
+
+        public void VisualiseConnections() {
+            connPanel.Children.Clear();
+            List<List<string>> conns = getConnections();
+            foreach (List<string> conn in conns) {
+                string type = conn[0];
+                string name = conn[1];
+                string addr = conn[2];
+                //Setting up border.
+                Border connBorder = new Border();
+                connBorder.BorderThickness = new Thickness(2,2,2,2);
+                connBorder.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#666666"));
+                connBorder.SetResourceReference(ContentControl.ContentProperty, "MaterialDesignToolBarMainPanelBorderStyle");
+                //Setting up the grid.
+                Grid connectionGrid = new Grid();
+                ColumnDefinition firstCol = new ColumnDefinition() { Width=new GridLength(200) };
+                ColumnDefinition secondCol = new ColumnDefinition();
+                ColumnDefinition thirdCol = new ColumnDefinition();
+                ColumnDefinition fourthCol = new ColumnDefinition() { Width = new GridLength(20) };
+                connectionGrid.ColumnDefinitions.Add(firstCol);
+                connectionGrid.ColumnDefinitions.Add(secondCol);
+                connectionGrid.ColumnDefinitions.Add(thirdCol);
+                connectionGrid.ColumnDefinitions.Add(fourthCol);
+                RowDefinition firstRow = new RowDefinition();
+                connectionGrid.RowDefinitions.Add(firstRow);
+                //Creating the icon.
+                FontAwesome.WPF.FontAwesome connIcn = new FontAwesome.WPF.FontAwesome();
+                connIcn.Foreground = new SolidColorBrush(Colors.White);
+                connIcn.Padding = new Thickness(20,20,20,20);
+                connIcn.FontSize = 80;
+                connIcn.Margin = new Thickness(0, 0, 10, 0);
+                connIcn.SetValue(Grid.ColumnProperty, 0);
+                if (type == "bluetooth")
+                {
+                    connIcn.Icon = FontAwesomeIcon.Bluetooth;
+                }
+                else {
+                    //Wifi
+                    connIcn.Icon = FontAwesomeIcon.Wifi;
+                }
+                //Creating the labels.
+                Label connNameLbl = new Label() { Name="connName", Content = name, Padding = new Thickness(0,30,0,0),  FontFamily = new FontFamily("Segoe UI Light"), FontSize = 23, Foreground = new SolidColorBrush(Colors.White) };
+                connNameLbl.SetValue(Grid.ColumnProperty, 1);
+                Label connAddrLbl = new Label() { Name = "connAddr", Content = addr, Padding = new Thickness(0, 60, 0, 0), FontFamily = new FontFamily("Segoe UI Light"), FontSize = 23, Foreground = new SolidColorBrush(Colors.White) };
+                connAddrLbl.SetValue(Grid.ColumnProperty, 1);
+                //Creating the buttons.
+                Button connectBtn = new Button() { Margin = new Thickness(0,-50,0,0), Name="connConnectBtn", HorizontalAlignment=HorizontalAlignment.Right, Width=120};
+                connectBtn.SetValue(Grid.ColumnProperty, 2);
+                connectBtn.SetResourceReference(ContentControl.ContentProperty, "MaterialDesignRaisedButton");
+                connectBtn.Content = "Connect";
+                Button forgetBtn = new Button() { Margin = new Thickness(0, 50, 0, 0), Name = "connForgetBtn", HorizontalAlignment = HorizontalAlignment.Right, Width = 120 };
+                forgetBtn.SetValue(Grid.ColumnProperty, 2);
+                forgetBtn.SetResourceReference(ContentControl.ContentProperty, "MaterialDesignRaisedButton");
+                forgetBtn.Content = "Forget";
+                connectionGrid.Children.Add(connectBtn);
+                connectionGrid.Children.Add(forgetBtn);
+                connectionGrid.Children.Add(connNameLbl);
+                connectionGrid.Children.Add(connAddrLbl);
+                connectionGrid.Children.Add(connIcn);
+                connBorder.Child = connectionGrid;
+                connPanel.Children.Add(connBorder);
+            }
         }
 
         private List<string> getAudioDevices() {
@@ -89,6 +172,7 @@ namespace openvoice_wpf
         private void createWifiConnBtn_Click(object sender, RoutedEventArgs e)
         {
             AddWifiWindow wifiWindow = new AddWifiWindow();
+            wifiWindow.Closed += onConnectionCreateWindow_Close;
             wifiWindow.Show();
         }
 
@@ -98,10 +182,16 @@ namespace openvoice_wpf
             try
             {
                 btWindow.Show();
+                btWindow.Closed += onConnectionCreateWindow_Close;
             }
             catch { 
                 //Meaning the bluetooth error occured in the AddBtWindow. Do nothing, the user is already informed.
             }
+        }
+
+        private void onConnectionCreateWindow_Close(object sender, EventArgs e)
+        {
+            VisualiseConnections();
         }
 
         private void changeAudioDevice(string name)
